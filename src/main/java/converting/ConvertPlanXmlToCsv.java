183c1fdc146d73b61b4c51a5f8388d4a5dd8c304
MahsaAll <mahsa.abdollahyar@gmail.com>
2024-09-15 20:49:04 +1000

    Fixing time format

--- src/main/java/converting/ConvertPlanXmlToCsv.java
+++ src/main/java/converting/ConvertPlanXmlToCsv.java
@@ -1,104 +1,122 @@
 package converting;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+/*-
+ * #%L
+ * Example Project
+ * %%
+ * Copyright (C) 2020 - 2024 by its authors.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.*;
+import org.matsim.core.config.Config;
+import org.matsim.core.population.io.PopulationReader;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.utils.misc.OptionalTime;
+
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.io.IOException;
 
 public class ConvertPlanXmlToCsv {
 
+    private static final String CSV_HEADER = "Person ID,Element Type,Activity Type,Link,X Coordinate,Y Coordinate," +
+            "End Time,Mode,Dep Time,Trav Time,Route Type,Start Link,End Link,Route Trav Time,Distance, Route";
+
     public static void main(String[] args) {
-        try {
-            File inputFile = new File(args[0]);  // input path eg: "./output_plan.xml"
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
-            doc.getDocumentElement().normalize();
+        if (args.length != 2) {
+            System.out.println(" Program requires two arguments:" +
+                    "1) input xml file path" +      // ./output_plans.xml
+                    "2) output csv file path");     // ./output_plans.csv
+            return;
+        }
 
-            PrintWriter writer = new PrintWriter(new FileWriter(args[1]));  // output path eg: "./output_plan.csv"
-            writer.println("Person ID,Element Type, Activity Type,Link,X Coordinate,Y Coordinate,End Time,Mode,Dep Time,Trav Time,Route Type,Start Link,End Link,Route Trav Time,Distance,Vehicle Ref ID, Route");
+        String inputFilePath = args[0];
+        String outputFilePath = args[1];
 
-            NodeList nList = doc.getElementsByTagName("person");
-            System.out.println("Total persons: " + nList.getLength());
+        Config config = ConfigUtils.createConfig();
+        Scenario scenario = ScenarioUtils.createScenario(config);
+        PopulationReader populationReader = new PopulationReader(scenario);
+        populationReader.readFile(inputFilePath);
 
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-                Node nNode = nList.item(temp);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    String personId = eElement.getAttribute("id");
-
-                    // Check for selected plans
-                    NodeList plans = eElement.getElementsByTagName("plan");
-                    for (int planIndex = 0; planIndex < plans.getLength(); planIndex++) {
-                        Element plan = (Element) plans.item(planIndex);
-                        if (plan.getAttribute("selected").equals("yes")) {
-                            processActivities(writer, plan, personId);
-                            processLegs(writer, plan, personId);
-                        }
-                    }
+        try (PrintWriter writer = new PrintWriter(new FileWriter(outputFilePath))) {
+            writer.println(CSV_HEADER);
+            for (Person person : scenario.getPopulation().getPersons().values()) {
+                Plan selectedPlan = person.getSelectedPlan();
+                if (selectedPlan != null) {
+                    processPlan(writer, person.getId().toString(), selectedPlan);
                 }
             }
-            writer.close();
             System.out.println("Finished writing to CSV");
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Error writing CSV: " + e.getMessage());
         }
     }
 
-    private static void processActivities(PrintWriter writer, Element planElement, String personId) {
-        NodeList activities = planElement.getElementsByTagName("activity");
-        for (int count = 0; count < activities.getLength(); count++) {
-            Node node = activities.item(count);
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                Element activity = (Element) node;
-                writer.printf("%s,Activity,%s,%s,%s,%s,%s,,,,,,,,,,\n",
-                        personId,
-                        activity.getAttribute("type"),
-                        activity.getAttribute("link"),
-                        activity.getAttribute("x"),
-                        activity.getAttribute("y"),
-                        activity.getAttribute("end_time"));
+    private static void processPlan(PrintWriter writer, String personId, Plan plan) {
+        for (PlanElement element : plan.getPlanElements()) {
+            if (element instanceof Activity) {
+                processActivity(writer, personId, (Activity) element);
+            } else if (element instanceof Leg) {
+                processLeg(writer, personId, (Leg) element);
             }
         }
     }
 
-    private static void processLegs(PrintWriter writer, Element planElement, String personId) {
-        NodeList legs = planElement.getElementsByTagName("leg");
-        for (int count = 0; count < legs.getLength(); count++) {
-            Node node = legs.item(count);
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                Element leg = (Element) node;
-                NodeList routes = leg.getElementsByTagName("route");
-                if (routes.getLength() > 0) {
-                    Element route = (Element) routes.item(0);
-                    writer.printf("%s,Leg,,,,,,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
-                            personId,
-                            leg.getAttribute("mode"),
-                            leg.getAttribute("dep_time"),
-                            leg.getAttribute("trav_time"),
-                            route.getAttribute("type"),
-                            route.getAttribute("start_link"),
-                            route.getAttribute("end_link"),
-                            route.getAttribute("trav_time"),
-                            route.getAttribute("distance"),
-                            route.getAttribute("vehicleRefId"),
-                            route.getTextContent().trim());
-                } else {
-                    // Handle legs without routes
-                    writer.printf("%s,Leg,,,,%s,%s,%s,,,,,,,\n",
-                            personId,
-                            leg.getAttribute("mode"),
-                            leg.getAttribute("dep_time"),
-                            leg.getAttribute("trav_time"));
-                }
-            }
+    private static void processActivity(PrintWriter writer, String personId, Activity activity) {
+        writer.printf("%s,Activity,%s,%s,%s,%s,%s,,,,,,,,,,\n",
+                personId,
+                activity.getType(),
+                activity.getLinkId(),
+                activity.getCoord().getX(),
+                activity.getCoord().getY(),
+                formatOptionalTime(activity.getEndTime()));
+    }
+
+    private static void processLeg(PrintWriter writer, String personId, Leg leg) {
+        Route route = leg.getRoute();
+        writer.printf("%s,Leg,,,,,,%s,%s,%s,%s,%s,%s,%s,%s,%s,\n",
+                personId,
+                leg.getMode(),
+                formatOptionalTime(leg.getDepartureTime()),
+                formatOptionalTime(leg.getTravelTime()),
+                route.getRouteType(),
+                route.getStartLinkId(),
+                route.getEndLinkId(),
+                formatOptionalTime(route.getTravelTime()),
+                route.getDistance(),
+                route.getRouteDescription());
+    }
+
+    private static String formatOptionalTime(OptionalTime optionalTime) {
+        if (optionalTime.isDefined()) {
+            long seconds = (long) optionalTime.seconds();
+            return formatSecondsToHHMMSS(seconds);
+        } else {
+            return "";
         }
     }
 
+    private static String formatSecondsToHHMMSS(long seconds) {
+        long hours = seconds / 3600;
+        long minutes = (seconds % 3600) / 60;
+        long secs = seconds % 60;
+        return String.format("%02d:%02d:%02d", hours, minutes, secs);
+    }
 }
